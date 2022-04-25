a67010c551d0e0c36b47fc2da735cc6188d349ec
paul097958 <paul097958@gmail.com>
2022-04-25 12:03:11 +0800

    fix

--- index.js
+++ index.js
@@ -16,36 +16,5 @@ app.get('/index.css', (req, res) => {
     res.sendFile(path.resolve('./src/index.css'))
 })
 
-const server = require('http').createServer();
-const io = require('socket.io')(server, {
-    cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
-    }
-});
-
-
-let online = 0;
-let db = []
-
-io.on('connection', client => {
-    online++
-    console.log(online, 'people online');
-    io.emit('sendOnlinePeople', online);
-    io.emit('reloadText', db)
-    client.on('newText', data => {
-        console.log('Some one say:', data);
-        db.unshift(data)
-        io.emit('reloadText', db)
-    })
-    client.on('disconnect', () => {
-        online--
-        console.log(online, 'people online');
-        io.emit('sendOnlinePeople', online)
-    });
-});
-
-
-// io.listen(8000);
 
 app.listen(3000);
